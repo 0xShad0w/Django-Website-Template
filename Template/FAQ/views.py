92537b5db5f937c18f2a4eee26cf5f7a28ca5c0e
David Mendes <mendesdd55@gmail.com>
2021-03-13 20:01:33 +0000

    FAQ view

--- Template/FAQ/views.py
+++ Template/FAQ/views.py
@@ -1,5 +1,16 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Topic, Question
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the FAQ index.")
+    
+    topic_questions = {}
+    
+    topics = Topic.objects.all()
+    
+    for t in topics:
+        questions = Question.objects.filter(question_topic_id=t)
+        topic_questions[t] = questions
+    
+    context = {'topic_questions': topic_questions}
+    return render(request, 'FAQ/index.html', context)
